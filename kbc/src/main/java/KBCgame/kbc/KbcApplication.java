31fc68a4bd4cafce00344f83e2e4ef7bc3b5f20d
suhanarao <45141709+suhanarao@users.noreply.github.com>
2020-08-31 23:10:49 +0530

    Add files via upload
    first commit

---
+++ kbc/src/main/java/KBCgame/kbc/KbcApplication.java
@@ -0,0 +1,13 @@
+package KBCgame.kbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbcApplication.class, args);
+	}
+
+}
---
+++ kbc/src/test/java/KBCgame/kbc/KbcApplicationTests.java
@@ -0,0 +1,13 @@
+package KBCgame.kbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
